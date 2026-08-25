620f17711432f539ab62c5bea1a96826ac385bf3
Loic <>
2026-08-25 19:08:58 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -1,6 +1,7 @@
 async function ini() {
   const myPonyCard = cards?.MainCharacter?.[0]
   const myPonyCardData = functions.getCardData(myPonyCard)
+  console.log(myPonyCard, myPonyCardData, cards)
   if (myPonyCardData) {
     game.data.Character.image = myPonyCardData.face.front.image
     game.data.Character.color = "#ffffff"
