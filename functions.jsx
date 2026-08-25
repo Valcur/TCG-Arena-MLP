de0249046dfca67117e403d892b93572261a9706
Loic <>
2026-08-25 19:40:17 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -1,9 +1,19 @@
 async function ini() {
   const myPonyCard = cards?.MainCharacter?.[0]
   const myPonyCardData = functions.getCardData(myPonyCard)
-  console.log(myPonyCard, myPonyCardData, cards)
   if (myPonyCardData) {
     game.data.Character.image = myPonyCardData.face.front.image
-    game.data.Character.color = "#ffffff"
+
+    const ponyName = myPonyCardData.name?.toLowerCase().replace(/\s+/g, "")
+    game.data.Character.color = colors[ponyName] ?? colors.twilight
   }
+}
+
+const colors = {
+  "twilight": "#A46BBD",
+  "applejack": "#EF6F2F",
+  "fluttershy": "#E9D461",
+  "pinkiepie": "#E880B0",
+  "rainbowdash": "#6BABDA",
+  "rarity": "#BDC1C2"
 }
