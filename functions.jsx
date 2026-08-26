7a746fa8ec1e89b715709b364685f8c59f9015e5
Loic <>
2026-08-26 17:33:23 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -5,6 +5,7 @@ async function ini() {
     game.data.Character.image = myPonyCardData.face.front.image
 
     const ponyName = myPonyCardData.name?.toLowerCase().replace(/\s+/g, "")
+    console.log(ponyName, myPonyCardData)
     const colorKey = Object.keys(colors).find(key => ponyName?.includes(key))
     game.data.Character.color = colors[colorKey] ?? colors.twilight
   }
