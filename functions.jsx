6b0e7d4e1228fe0e0b2afc8be8354ac2d6915b15
Loic <>
2026-08-28 13:14:27 +0200

    no message

--- functions.jsx
+++ functions.jsx
@@ -5,10 +5,26 @@ async function ini() {
     game.data.Character.image = myPonyCardData.face.front.image
 
     const ponyName = myPonyCardData.name?.toLowerCase().replace(/\s+/g, "")
-    console.log(ponyName, myPonyCardData) 
     const colorKey = Object.keys(colors).find(key => ponyName?.includes(key))
     game.data.Character.color = colors[colorKey] ?? colors.twilight
   }
+}
+
+async function onPonyEnter(section, ponies) {
+  if (ponies.length <= 0) return
+  const sectionData = game.data[section + "_Boost"]
+  console.log(sectionData, section, ponies)
+  if (!sectionData) return
+  const ponyCard = ponies[0]
+  const ponyCardData = functions.getCardData(ponyCard)
+  sectionData.boost = 4
+}
+
+async function onPonyLeft(section) {
+  const sectionData = game.data[section + "_Boost"]
+  console.log(sectionData, section)
+  if (!sectionData) return
+  sectionData.boost = 0
 }
 
 const colors = {
